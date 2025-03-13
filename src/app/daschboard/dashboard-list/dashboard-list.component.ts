f1b0b320553c2d85aa02f6c2e5743679fbeccee5
Verboven Koen <koenverboven@gmail.com>
2025-03-13 09:20:55 +0100

    adding mock for dashboard

--- src/app/daschboard/dashboard-list/dashboard-list.component.ts
+++ src/app/daschboard/dashboard-list/dashboard-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {DashboardItem} from '../../../models/dashboardItem'
 import { CommonModule } from '@angular/common';
+import {dashboarditems} from '../mock-dashboard-list';
 
 @Component({
   selector: 'app-dashboard-list',
@@ -10,43 +10,6 @@ import { CommonModule } from '@angular/common';
   styleUrl: './dashboard-list.component.css'
 })
 export class DashboardListComponent {
-    items: DashboardItem[] = [
-      {
-        id:1,
-        itemName:'Students',
-        columnNumber: 1,
-        itemCount:0,
-        color:'red'
-      },
-      {
-        id:2,
-        itemName:'Teachers',
-        columnNumber: 1,
-        itemCount:0,
-        color:'red'
-      },
-      {
-        id:3,
-        itemName:'Courses',
-        columnNumber: 3,
-        itemCount:0,
-        color:'green'
-      },
-      {
-        id:4,
-        itemName:'Exams',
-        columnNumber: 2,
-        itemCount:0,
-        color:'blue'
-      },
-      {
-        id:5,
-        itemName:'ExamResults',
-        columnNumber: 2,
-        itemCount:0,
-        color:'blue'
-      },
-      
-    ];
+    items =  dashboarditems ;   
    
 }
---
+++ src/app/daschboard/mock-dashboard-list.ts
@@ -0,0 +1,39 @@
+import {DashboardItem} from '../../models/dashboardItem'
+
+  export const  dashboarditems: DashboardItem[] = [
+    {
+      id:1,
+      itemName:'Students',
+      columnNumber: 1,
+      itemCount:0,
+      color:'red'
+    },
+    {
+      id:2,
+      itemName:'Teachers',
+      columnNumber: 1,
+      itemCount:0,
+      color:'red'
+    },
+    {
+      id:3,
+      itemName:'Courses',
+      columnNumber: 3,
+      itemCount:0,
+      color:'green'
+    },
+    {
+      id:4,
+      itemName:'Exams',
+      columnNumber: 2,
+      itemCount:0,
+      color:'blue'
+    },
+    {
+      id:5,
+      itemName:'ExamResults',
+      columnNumber: 2,
+      itemCount:0,
+      color:'blue'
+    },
+]
