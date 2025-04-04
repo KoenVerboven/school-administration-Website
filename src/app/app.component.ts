86deb92a784720da6b5720767ca49530e93104d7
Verboven Koen <koenverboven@gmail.com>
2025-04-04 11:55:13 +0200

    Adding sidbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule], 
+  imports: [RouterOutlet, RouterModule, SidebarComponent,CommonModule], 
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   public mySchoolName = 'Free univerity';
+  sidebarExpanded = true;
 }
