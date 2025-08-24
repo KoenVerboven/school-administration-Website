ab8ec60579ae08ba397535d4634e9a95d82a67a1
Verboven Koen <koenverboven@gmail.com>
2025-08-24 06:59:41 +0200

    feat: refine dashboard item styles and update mock data for improved layout and functionality

--- src/app/dashboard/dashboard-list/mock-dashboard-list.ts
+++ src/app/dashboard/dashboard-list/mock-dashboard-list.ts
@@ -13,7 +13,7 @@ import {DashboardItem} from '../models/dashboardItem.model'
     {
       id:2,
       itemName:'Teachers',
-      columnNumber: 1,
+      columnNumber: 2,
       itemCount:0,
       color:'#b235ecff',
       url:'/teachers',
@@ -22,7 +22,7 @@ import {DashboardItem} from '../models/dashboardItem.model'
     {
       id:3,
       itemName:'Courses',
-      columnNumber: 2,
+      columnNumber: 3,
       itemCount:0,
       color:'#1eb918ff',
       url:'/courses',
@@ -39,8 +39,26 @@ import {DashboardItem} from '../models/dashboardItem.model'
     },
     {
       id:5,
-      itemName:'Users',
+      itemName:'Teachers Present',
       columnNumber: 2,
+      itemCount: 0,
+      color:'#b235ecff',
+      url:'/users',
+      visible:true
+    },
+     {
+      id:6,
+      itemName:'Students Present',
+      columnNumber: 1,
+      itemCount: 0,
+      color:'#eb4040ff',
+      url:'/studentsPresent',
+      visible:true
+    },
+     {
+      id:7,
+      itemName:'Users',
+      columnNumber: 1,
       itemCount: 0,
       color:'#1bbcc2ff',
       url:'/users',
